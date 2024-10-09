eb95c9f9d9302d1b3928fea61e4ca354ac2d6324
jerryy-maker <lady-lei@foxmail.com>
2024-10-09 16:30:26 +0800

    threads

--- exercises/threads/threads3.rs
+++ exercises/threads/threads3.rs
@@ -3,7 +3,7 @@
 // Execute `rustlings hint threads3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 use std::sync::mpsc;
 use std::sync::Arc;
@@ -26,31 +26,36 @@ impl Queue {
     }
 }
 
-fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> () {
-    let qc = Arc::new(q);
-    let qc1 = Arc::clone(&qc);
-    let qc2 = Arc::clone(&qc);
+fn send_tx(q: Arc<Queue>, tx: mpsc::Sender<u32>) {
+    let q_clone1 = Arc::clone(&q);
+    let tx_clone = tx.clone();
+    
 
-    thread::spawn(move || {
-        for val in &qc1.first_half {
+    let handle1 = thread::spawn(move || {
+        for val in &q_clone1.first_half {
+            println!("sending {:?}", val);
+            tx_clone.send(*val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    let q_clone2 = Arc::clone(&q);
+
+    let handle2 = thread::spawn(move || {
+        for val in &q_clone2.second_half {
             println!("sending {:?}", val);
             tx.send(*val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
 
-    thread::spawn(move || {
-        for val in &qc2.second_half {
-            println!("sending {:?}", val);
-            tx.send(*val).unwrap();
-            thread::sleep(Duration::from_secs(1));
-        }
-    });
+    handle1.join().unwrap();
+    handle2.join().unwrap();
 }
 
 fn main() {
     let (tx, rx) = mpsc::channel();
-    let queue = Queue::new();
+    let queue = Arc::new(Queue::new());
     let queue_length = queue.length;
 
     send_tx(queue, tx);
